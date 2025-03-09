6c1057a445bca646369b910904df563646355964
Myself-Abdullah-Rajput <rajputxbrand117@gmail.com>
2025-03-09 23:09:43 +0500

    Added Ad Config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Cot } from "@/components/Cot";
+
+<simport { Cot } from "@/components/Cot";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
@@ -12,6 +13,14 @@ export default function Home() {
       <Testimonials />
       <Cot />
       <Footer />
+	  <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16861373148"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'AW-16861373148');
+		</script>
     </>
   );
 }
