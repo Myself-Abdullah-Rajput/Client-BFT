2c7ec0e8b4798b3876a617604871d5e32ed6c8e8
Myself-Abdullah-Rajput <rajputxbrand117@gmail.com>
2025-03-09 23:14:08 +0500

    Bug Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,27 @@ import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import Testimonials from "@/components/Testimonials";
+import Head from "next/head"; // Import Head
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16861373148"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16861373148');
+          `}
+        </script>
+      </Head>
       <Header />
       <Hero />
       <Testimonials />
       <Cot />
       <Footer />
-	  
-	  <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16861373148"></script>
-		<script>
-		  window.dataLayer = window.dataLayer || [];
-		  function gtag(){dataLayer.push(arguments);}
-		  gtag('js', new Date());
-
-		  gtag('config', 'AW-16861373148');
-	  </script>
     </>
   );
 }
