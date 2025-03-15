c5f6291d1521dfaeb47b96bd0e1b2491eafa7c74
Myself-Abdullah-Rajput <rajputxbrand117@gmail.com>
2025-03-15 21:57:27 +0500

    Improved the email template design

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -22,20 +22,34 @@ export async function POST(request: NextRequest) {
     },
   });
 
-  const mailOptions: Mail.Options = {
+const mailOptions: Mail.Options = {
     from: process.env.NEXT_EMAIL_ADDRESS,
     to: process.env.NEXT_EMAIL_ADDRESS,
     // cc: email, (uncomment this line if you want to send a copy to the sender)
     subject: `New Query from (${email})`,
-    text: `
-        Email Address: ${email}
-        Phone: ${phone}
-        Departure Destination: ${depDest}
-        Arrival Destination: ${arrDest}
-        Departure Date: ${depDate}
-        Return Date: ${retDate}
-    `,
-  };
+    html: `
+        <div style="width: 100%; display: flex; align-items: center; justify-content: center; padding: 20px 20px; background-color: #f8f9fa;">
+            <div style="border: 1px solid #dee2e6; border-radius: 8px; padding: 24px; text-align: center; max-width: 600px; width:90%; background-color: #ffffff; font-family: 'Arial', sans-serif;">
+                <div style="font-size: 28px; font-weight: 600; color: #343a40; margin-bottom: 12px;">
+                    From ${depDest} To ${arrDest}
+                </div>
+                <p style="color: #495057; font-size: 16px; margin-bottom: 16px;">${depDate}</p>
+                <div style="text-align: left; margin-bottom: 16px;">
+                    <label style="color: #212529; font-weight: 600; font-size: 16px; display: block; margin-bottom: 4px;">Return Date:</label>
+                    <p style="color: #495057; font-size: 16px; margin: 0;">${retDate}</p>
+                </div>
+                <div style="text-align: left; margin-bottom: 16px;">
+                    <label style="color: #212529; font-weight: 600; font-size: 16px; display: block; margin-bottom: 4px;">Phone:</label>
+                    <p style="color: #495057; font-size: 16px; margin: 0;">${phone}</p>
+                </div>
+                <div style="text-align: left; margin-bottom: 16px;">
+                    <label style="color: #212529; font-weight: 600; font-size: 16px; display: block; margin-bottom: 4px;">Email:</label>
+                    <p style="color: #495057; font-size: 16px; margin: 0;">${email}</p>
+                </div>
+            </div>
+        </div>
+    `
+};
 
   const sendMailPromise = () =>
     new Promise<string>((resolve, reject) => {
